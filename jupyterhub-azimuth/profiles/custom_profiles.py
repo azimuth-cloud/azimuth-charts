61052a49417ff1eb06b00e5ba102bff00605f8b4
Jasleen <jasleen@stackhpc.com>
2025-09-02 13:43:57 +0100

    review changes

--- jupyterhub-azimuth/profiles/custom_profiles.py
+++ jupyterhub-azimuth/profiles/custom_profiles.py
@@ -6,28 +6,19 @@ def get_profile_list(spawner):
     logging.warning("Loading custom profiles for JupyterHub.")
     profiles = []
 
-    custom_profiles = yaml.safe_load("""\n{{ .Values.user_notebook_profiles.default_profiles | toYaml }}""")
-    if custom_profiles:
-        profiles.extend(custom_profiles)
+    default_profiles = yaml.safe_load("""\n{{ .Values.user_notebook_profiles.default_profiles | toYaml }}""")
+    if default_profiles:
+        profiles.extend(default_profiles)
+
     config.load_incluster_config()
     api = client.CoreV1Api()
     nodes = api.list_node().items
 
-    device_id_intel_gpu = "{{  .Values.conditions.device_id_intel_gpu }}" + ".present"
-    nvidia_gpu = "{{  .Values.conditions.nvidia_gpu }}" + ".present"
-
-    has_nvidia_gpu = lambda node: node.metadata.labels.get(nvidia_gpu, "") == "true"
-    has_intel_gpu = lambda node: node.metadata.labels.get(device_id_intel_gpu, "") == "true"
-
-    if any(map(has_nvidia_gpu, nodes)):
-        profiles.extend(yaml.safe_load("""\n{{ .Values.user_notebook_profiles.nvidia_gpu | toYaml }}"""))
-    else:
-        logging.warning("No Nvidia GPU nodes found, skipping Pytorch Nvidia GPU profile.")
-
-    if any(map(has_intel_gpu, nodes)):
-        profiles.extend(yaml.safe_load("""\n{{ .Values.user_notebook_profiles.intel_gpu | toYaml }}"""))
-    else:
-        logging.warning("No Intel GPU nodes found, skipping profile.")    
-    logging.warning(f"Available profiles: {[profile['display_name'] for profile in profiles]}")
+    custom_profile = yaml.safe_load("""\n{{ .Values.user_notebook_profiles.custom_profiles | toYaml }}""")
+    for label_key, profile_definition in custom_profile.items():
+        has_profile = lambda node: node.metadata.labels.get(label_key, "") == "true"
+        if any(map(has_profile, nodes)):
+            profiles.extend(profile_definition)
+            logging.info(f"Added custom profile for label {label_key}.")
 
     return profiles
