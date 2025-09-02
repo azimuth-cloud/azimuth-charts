9135342eff88902ec625aff95ddb8d513ee89ad7
Jasleen <jasleen@stackhpc.com>
2025-09-02 15:34:18 +0100

    Review changes

--- jupyterhub-azimuth/profiles/custom_profiles.py
+++ jupyterhub-azimuth/profiles/custom_profiles.py
@@ -3,17 +3,22 @@ from kubernetes import client, config
 import os, yaml
 
 def get_profile_list(spawner):
-    logging.warning("Loading custom profiles for JupyterHub.")
+    logging.info("Loading custom profiles for JupyterHub.")
     profiles = []
 
+    # Include any default profiles provided via Helm values
     default_profiles = yaml.safe_load("""\n{{ .Values.user_notebook_profiles.default_profiles | toYaml }}""")
     if default_profiles:
         profiles.extend(default_profiles)
 
+    # Check for certain types of specialised hardware on the host cluster (e.g. GPUs) 
+    # and add some appropriate notebook profiles
     config.load_incluster_config()
     api = client.CoreV1Api()
     nodes = api.list_node().items
 
+    # Include any default profiles provided via Helm values by finding if the
+    # specific label is present on any node in the cluster
     custom_profile = yaml.safe_load("""\n{{ .Values.user_notebook_profiles.custom_profiles | toYaml }}""")
     for label_key, profile_definition in custom_profile.items():
         has_profile = lambda node: node.metadata.labels.get(label_key, "") == "true"
