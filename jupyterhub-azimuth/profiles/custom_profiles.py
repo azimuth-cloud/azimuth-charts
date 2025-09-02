073c0b2b3b34663286804df3d994a8dc669c7b6a
Jasleen <jasleen@stackhpc.com>
2025-09-02 15:20:10 +0100

    Modify according to changes requested

--- jupyterhub-azimuth/profiles/custom_profiles.py
+++ jupyterhub-azimuth/profiles/custom_profiles.py
@@ -1,37 +1,24 @@
 import logging
 from kubernetes import client, config
-import os, yaml
+import os,yaml
 
 def get_profile_list(spawner):
-    logging.info("Loading custom profiles for JupyterHub.")
+    logging.warning("Loading custom profiles for JupyterHub.")
     profiles = []
 
-    # Include any custom profiles provided via Helm values
-    custom_profiles = yaml.safe_load("""\n{{ .Values.user_notebook_profiles.default_profiles | toYaml }}""")
-    if custom_profiles:
-        profiles.extend(custom_profiles)
-    
-    # Check for certain types of specialised hardware on the host cluster (e.g. GPUs) 
-    # and add some appropriate notebook profiles
+    default_profiles = yaml.safe_load("""\n{{ .Values.user_notebook_profiles.default_profiles | toYaml }}""")
+    if default_profiles:
+        profiles.extend(default_profiles)
+
     config.load_incluster_config()
     api = client.CoreV1Api()
     nodes = api.list_node().items
 
-    device_id_intel_gpu = "{{  .Values.conditions.device_id_intel_gpu }}" + ".present"
-    nvidia_gpu = "{{  .Values.conditions.nvidia_gpu }}" + ".present"
-
-    has_nvidia_gpu = lambda node: node.metadata.labels.get(nvidia_gpu, "") == "true"
-    has_intel_gpu = lambda node: node.metadata.labels.get(device_id_intel_gpu, "") == "true"
-
-    if any(map(has_nvidia_gpu, nodes)):
-        profiles.extend(yaml.safe_load("""\n{{ .Values.user_profile_values.nvidia_gpu | toYaml }}"""))
-    else:
-        logging.info("No Nvidia GPU nodes found, skipping Pytorch Nvidia GPU profile.")
-
-    if any(map(has_intel_gpu, nodes)):
-        profiles.extend(yaml.safe_load("""\n{{ .Values.user_profile_values.intel_gpu | toYaml }}"""))
-    else:
-        logging.info("No Intel GPU nodes found, skipping profile.")    
-    logging.info(f"Available profiles: {[profile.get('display_name', 'unknown') for profile in profiles]}")
+    custom_profile = yaml.safe_load("""\n{{ .Values.user_notebook_profiles.custom_profiles | toYaml }}""")
+    for label_key, profile_definition in custom_profile.items():
+        has_profile = lambda node: node.metadata.labels.get(label_key, "") == "true"
+        if any(map(has_profile, nodes)):
+            profiles.extend(profile_definition)
+            logging.info(f"Added custom profile for label {label_key}.")
 
     return profiles
