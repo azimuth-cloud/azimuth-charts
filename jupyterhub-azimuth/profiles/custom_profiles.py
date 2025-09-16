b3151eceee217cf755efc5b5435f2545a88282a5
Jasleen <jasleen@stackhpc.com>
2025-09-16 14:34:44 +0100

    updating profile-list

--- jupyterhub-azimuth/profiles/custom_profiles.py
+++ jupyterhub-azimuth/profiles/custom_profiles.py
@@ -16,9 +16,12 @@ def get_profile_list(spawner):
     config.load_incluster_config()
     api = client.CoreV1Api()
     nodes = api.list_node().items
+<<<<<<< Updated upstream
 
     # Include any default profiles provided via Helm values by finding if the
     # specific label is present on any node in the cluster
+=======
+>>>>>>> Stashed changes
     custom_profile = yaml.safe_load("""\n{{ .Values.user_notebook_profiles.custom_profiles | toYaml }}""")
     for label_key, profile_definition in custom_profile.items():
         has_profile = lambda node: node.metadata.labels.get(label_key, "") == "true"
