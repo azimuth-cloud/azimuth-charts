fc9bb64af86c3ec32e98d3c4f2cc9ab7b0045185
Matt Crees <mattc@stackhpc.com>
2026-07-09 11:00:07 +0100

    Add AMD JupyterHub environment
    Also changes the custom profiles lookup to check for tag presence,
    rather than value. This is because AMD doesn't provide a generic
    presence label, so every value will be specific to the GPU
    generation in use.
    
    Uses the image from:
    https://github.com/stackhpc/jupyterhub-pytorch-amd-gpu

--- jupyterhub-azimuth/profiles/custom_profiles.py
+++ jupyterhub-azimuth/profiles/custom_profiles.py
@@ -18,11 +18,10 @@ def get_profile_list(spawner):
     # Include any custom profiles provided via Helm values
     custom_profiles = yaml.safe_load("""\n{{ .Values.user_notebook_profiles.custom_profiles | toYaml }}""")
     for label_key, profile_definition in custom_profiles.items():
-        label_value = profile_definition["label_value"]
         extra_profiles = profile_definition["profiles"]        
-        has_node_label = lambda node: node.metadata.labels.get(label_key) == label_value
+        has_node_label = lambda node: label_key in node.metadata.labels
         if any(map(has_node_label, nodes)):
             profiles.extend(extra_profiles)
-            logging.info(f"Added custom profile for label {label_key} == {label_value}.")
+            logging.info(f"Added custom profile for label {label_key}.")
 
     return profiles
